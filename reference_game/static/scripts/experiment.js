256233f3c38123861379ba74fc14557335813ae4
Robert Hawkins <rxdh@stanford.edu>
2019-10-31 02:28:55 -0400

    convert client-side to es6 OO to allow persistent vars

--- reference_game/static/scripts/experiment.js
+++ reference_game/static/scripts/experiment.js
@@ -1,123 +1,146 @@
 /*globals $, dallinger */
-var my_node_id;
-var ws_scheme = (window.location.protocol === "https:") ? 'wss://' : 'ws://';
-var socket = new ReconnectingWebSocket(
-  ws_scheme + window.location.host + "/chat?channel=chat"
-);
+const ws_scheme = (window.location.protocol === "https:") ? 'wss://' : 'ws://';
 
-// Create the agent.
-var create_agent = function () {
-  dallinger.createAgent()
-    .done(function (resp) {
-      my_node_id = resp.node.id;
-      console.log(my_node_id);
-      $("#chat-history").show();
-      $("#response-form").show();
-      $("#send-message").removeClass("disabled");
-      $("#send-message").html("Send");
-      $("#reproduction").focus();
-    })
-    .fail(function (rejection) {
-      // A 403 is our signal that it's time to go to the questionnaire
-      if (rejection.status === 403) {
-        dallinger.allowExit();
-        dallinger.goToPage('questionnaire');
-      } else {
-        dallinger.error(rejection);
-      }
+class CoordinationChatRoomClient {
+  constructor() {
+    this.socket = new ReconnectingWebSocket(
+      ws_scheme + window.location.host + "/chat?channel=chat"
+    );
+    this.my_node_id = '';
+    this.im_old = 'blah blah blah';
+    this.messageSent = false;
+    this.alreadyClicked = false;
+    this.createAgent();
+    this.initializeStimGrid();
+    this.setupHandlers();
+  }
+
+  createAgent() {
+    self = this;
+    dallinger.createAgent()
+      .done(resp => {
+	self.my_node_id = resp.node.id;
+	console.log(this);
+	console.log('done changing node id');
+	$("#chat-history").show();
+	$("#response-form").show();
+	$("#send-message").removeClass("disabled");
+	$("#send-message").html("Send");
+	$("#reproduction").focus();
+      })
+      .fail(rejection => {
+	// A 403 is our signal that it's time to go to the questionnaire
+	if (rejection.status === 403) {
+          dallinger.allowExit();
+          dallinger.goToPage('questionnaire');
+	} else {
+          dallinger.error(rejection);
+	}
+      });
+  }
+
+  initializeStimGrid() {
+    const currStim = [
+      {url: 'tangram_A.png', targetStatus: 'target'},
+      {url: 'tangram_B.png', targetStatus: 'distractor1'},
+      {url: 'tangram_C.png', targetStatus: 'distractor2'}
+    ];
+    _.forEach(currStim, (stim, i) => {
+      const bkg = 'url(./static/images/' + stim.url + ')';
+      const div = $('<div/>')
+	  .addClass('pressable')
+	  .attr({'id' : stim.targetStatus})
+	  .css({'background' : bkg})
+	  .css({
+	    'position': 'relative',
+	    'grid-row': 1, 'grid-column': i+1,
+	    'background-size' :'cover'
+	  });
+      $("#object-grid").append(div);
     });
-};
+  };
 
-var initialize_grid = function() {
-  var currStim = [
-    {url: 'tangram_A.png', targetStatus: 'target'},
-    {url: 'tangram_B.png', targetStatus: 'distractor1'},
-    {url: 'tangram_C.png', targetStatus: 'distractor2'}
-  ];
-  _.forEach(currStim, (stim, i) => {
-    var bkg = 'url(./static/images/' + stim.url + ')';
-    var div = $('<div/>')
-	.addClass('pressable')
-	.attr({'id' : stim.targetStatus})
-	.css({'background' : bkg})
-	.css({
-	  'position': 'relative',
-	  'grid-row': 1, 'grid-column': i+1,
-	  'background-size' :'cover'
-	});
-    $("#object-grid").append(div);
-  });
-};
+  handleChatReceived (msg) {
+    console.log(this);
+    this.messageSent = true;
+    console.log(this);
+    $("#story")
+      .append("<p>" + msg.content + "</p>")
+      .stop(true,true)
+      .animate({
+	scrollTop: $("#story").prop("scrollHeight")
+      }, 800);
+  }
 
-var display_info = function(msg) {
-  $("#story")
-    .append("<p>" + msg + "</p>")
-    .stop(true,true)
-    .animate({
-      scrollTop: $("#story").prop("scrollHeight")
-    }, 800);
-;
-};
+  sendMessage (msg) {
+    console.log(this);
+    this.im_old = 'not anymore';
+    $("#send-message").addClass("disabled");
+    $("#send-message").html("Sending...");
+    $("#reproduction").val("");
+    $("#reproduction").focus();
+    if(msg != '') {
+      this.socket.send('chat:' + JSON.stringify({
+	'type' : 'chatMessage', 'content' : msg
+      }));
+    }
+    $("#send-message").removeClass("disabled");
+    $("#send-message").html("Send");
+  }
 
-var send_message = function() {
-  $("#send-message").addClass("disabled");
-  $("#send-message").html("Sending...");
-
-  const response = $("#reproduction").val();
-  $("#reproduction").val("");
-  $("#reproduction").focus();
-
-  socket.send('chat:' + JSON.stringify({
-    'type' : 'message', 'content' : response
-  }));
+  sendResponse(id) {
+    console.log(this);
+    if(this.messageSent & !this.alreadyClicked) {
+      const clickedId = id;
+      this.alreadyClicked = true;
+      this.socket.send('chat:' + JSON.stringify({
+	'type' : 'clickedObj', 'objectID' : clickedId
+      }));
+    }
+  }
   
-  socket.send('chat:' + JSON.stringify({
-    'type' : 'clickedObj', 'objectID' : '1'
-  }));
-  $("#send-message").removeClass("disabled");
-  $("#send-message").html("Send");
-};
+  setupHandlers() {
+    // Handle messages sent from the server
+    self = this;
+    this.socket.onmessage = e => {
+      const rawMessage = e.data;
+      const handlers = {'chatMessage' : self.handleChatReceived.bind(this)};
+      if(rawMessage.startsWith("chat:")) {
+	const body = JSON.parse(rawMessage.replace("chat:", ""));
+	if(handlers.hasOwnProperty(body.type)) {
+	  handlers[body.type](body);
+	} else {
+	  console.log("Received mysterious message", rawMessage);
+	}
+      }
+    };
+    
+    // Send whatever is in the chatbox when button clicked
+    $("#send-message").click(() => {
+      const msg = $("#reproduction").val();
+      self.sendMessage(msg);
+    });
+    
+    $('div.pressable').click(event => {
+      // Only let listener click once they've heard answer back
+      self.sendResponse(event.target.id);
+    });
 
-var leave_chatroom = function() {
-  dallinger.goToPage("questionnaire");
-};
+    // Leave the chatroom.
+    $("#leave-chat").click(function() {
+      dallinger.goToPage("questionnaire");
+    });
+  }
+}
 
-$(document).keypress(function (e) {
+$(document).keypress(e => {
   if (e.which === 13) {
-    console.log("enter!");
     $("#send-message").click();
     return false;
   }
 });
 
-$(document).ready(function() {
-  socket.onmessage = function(e) {
-    var handlers = {'message' : (msg) => display_info(msg.content)};
-    var raw_message = e.data;
-    if(raw_message.startsWith("chat:")) {
-      console.log("We received a message for our channel: " + raw_message);
-      var body = JSON.parse(raw_message.replace("chat:", ""));
-      if(handlers.hasOwnProperty(body.type)) {
-	handlers[body.type](body);
-      } else {
-	console.log("Received mysterious message", raw_message);
-      }
-    }
-  };
-
-  // Send a message.
-  $("#send-message").click(function() {
-    send_message();
-  });
-
-  // Leave the chatroom.
-  $("#leave-chat").click(function() {
-    leave_chatroom();
-  });
-
-  // Proceed to the waiting room.
-  $("#go-to-waiting-room").click(function() {
-      dallinger.goToPage("waiting");
-  });
-
+$(document).ready(() => {
+  console.log('ready');
+  new CoordinationChatRoomClient();
 });
