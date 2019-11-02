39258a332eed76b7c51affaf06a4d82f990fcfbc
Robert Hawkins <rxdh@stanford.edu>
2019-11-02 02:45:29 -0400

    manage server-side list of connected players; send 'start' message once everyone connected

--- reference_game/experiment.py
+++ reference_game/experiment.py
@@ -8,12 +8,9 @@ from dallinger.compat import unicode
 from dallinger.config import get_config
 from dallinger.experiment import Experiment
 from dallinger.nodes import Agent
+from dallinger.models import Network, Node
 
-# try:
-#     from .bots import Bot
-#     Bot = Bot  # Make name "Bot" importable without triggering style warnings
-# except ImportError:
-#     pass
+from dallinger.db import redis_conn
 
 logger = logging.getLogger(__name__)
 
@@ -29,7 +26,8 @@ class CoordinationChatroom(Experiment):
     def __init__(self, session=None):
         """Initialize the experiment."""
         super(CoordinationChatroom, self).__init__(session)
-        self.channel = 'chat'
+        self.channel = 'refgame'
+        self.games = {}
         if session:
             self.setup()
 
@@ -38,6 +36,7 @@ class CoordinationChatroom(Experiment):
         self.experiment_repeats = repeats = config.get("repeats")
         self.network_class = config.get("network")
         self.quorum = config.get("n")
+
         # Recruit for all networks at once
         self.initial_recruitment_size = repeats * self.quorum
 
@@ -61,10 +60,36 @@ class CoordinationChatroom(Experiment):
 
     def handle_clicked_obj(self, msg) :
         print('handleing msg', msg)
-        
+
+    def handle_connect(self, msg):
+        # Once participant connects, add them to their respective game list
+        network_id = msg['network_id']
+        if network_id not in self.games :
+            self.games[network_id] = []
+            
+        connected_player_list = self.games[network_id]
+        connected_player_list.append(msg['participant_id'])
+
+        # Once everyone is properly connected (i.e. nodes added, etc), send message to commence play
+        if len(connected_player_list) == self.quorum :
+            network = Network.query.filter_by(id=msg['network_id'])
+            
+            # TODO: this would be a lot more elegant if diff networks had diff channels
+            # instead of sending everything through single channel (so everyone has to check if they're recipient)
+            logger.info('sending newRound packet')
+            redis_conn.publish('refgame', json.dumps({
+                'type': 'newRound',
+                'trialNum' : 1,
+                'networkid' : msg['network_id'],
+                'roles' : {'speaker' : connected_player_list[0], 'listener' : connected_player_list[1]}
+            }))
+            
     def send(self, raw_message) :
         """override default send to handle participant messages on channel"""
-        handlers = {'clickedObj' : self.handle_clicked_obj}
+        handlers = {
+            'connect' : self.handle_connect,
+            'clickedObj' : self.handle_clicked_obj
+        }
         if raw_message.startswith(self.channel + ":") :
             logger.info("We received a message for our channel: {}".format(
                 raw_message))
