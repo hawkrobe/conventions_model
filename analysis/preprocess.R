45b500365be9b038a0e8f1a32e4d18d4059fd591
Robert Hawkins <rxdh@stanford.edu>
2019-12-08 02:00:52 -0500

    Add analysis & preprocessingg script to compile raw data

---
+++ analysis/preprocess.R
@@ -0,0 +1,44 @@
+read_csv('./data/live11/participant.csv') %>%
+  rbind(read_csv('./data/live13/participant.csv')) %>%
+  mutate(duration = end_time - creation_time)
+
+messages1 <- read_csv('../data/live11/info.csv') %>%
+  filter(contents == 'chatMessage') %>%
+  pull(details) %>%
+  map_dfr(jsonlite::fromJSON)
+
+messages2 <- read_csv('../data/live13/info.csv') %>%
+  filter(contents == 'chatMessage') %>%
+  pull(details) %>%
+  map_dfr(jsonlite::fromJSON)%>% 
+  mutate(networkid = networkid + 20, 
+         participantid = participantid + 80)
+
+messages <- rbind(messages1, messages2) %>%
+  select(-aID, -wID, -type) %>%
+  arrange(networkid, roomid) 
+
+write_csv(messages, './data/messages.csv')
+
+clicks1 <- read_csv('../data/live11/info.csv') %>% 
+  filter(contents == 'clickedObj') %>%
+  pull(details) %>% 
+  map_dfr(jsonlite::fromJSON) 
+
+clicks2 <- read_csv('../data/live13/info.csv') %>% 
+  filter(contents == 'clickedObj') %>%
+  pull(details) %>% 
+  map_dfr(jsonlite::fromJSON) %>% 
+  mutate(networkid = networkid + 20, 
+         participantid = participantid + 80)
+
+clicks <- rbind(clicks1, clicks2) %>%
+  select(-aID, -wID, -type) %>%
+  arrange(networkid, roomid) %>%
+  mutate(bonus = round(bonus, 2))
+
+write_csv(clicks, './data/clicks.csv')
+
+connects <- read_csv('./data/live11/info.csv') %>%
+  rbind(read_csv('./data/live13/info.csv')) %>%
+  select(-workerid, -assignmentid)
