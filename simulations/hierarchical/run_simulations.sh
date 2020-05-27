47b0ec50c5a97c5fa3821ab40992165a5ae1e107
Robert Hawkins <hawkrobe@gmail.com>
2020-05-27 03:12:33 -0400

    run all simulations in one script...

--- simulations/hierarchical/run_simulations.sh
+++ simulations/hierarchical/run_simulations.sh
@@ -1,5 +1,5 @@
 #!/bin/bash#
 # parallel --bar --colsep ',' "sh ./run_simulations.sh {1} {2} {3} {4}" :::: input/reps.csv
-/mnt/bucket/people/robertdh/.webppl/webppl partnerspecificity_listener.wppl --require ./refModule/ --require webppl-csv -- --model $1 --alpha $2 --costWeight $3 --chainNum $4
-/mnt/bucket/people/robertdh/.webppl/webppl partnerspecificity_speaker.wppl --require ./refModule/ --require webppl-csv -- --model $1 --alpha $2 --costWeight $3 --chainNum $4
+# /mnt/bucket/people/robertdh/.webppl/webppl partnerspecificity_listener.wppl --require ./refModule/ --require webppl-csv -- --model $1 --alpha $2 --costWeight $3 --chainNum $4
+# /mnt/bucket/people/robertdh/.webppl/webppl partnerspecificity_speaker.wppl --require ./refModule/ --require webppl-csv -- --model $1 --alpha $2 --costWeight $3 --chainNum $4
 /mnt/bucket/people/robertdh/.webppl/webppl network_simulation.wppl --require ./refModule/ --require webppl-csv -- --model $1 --alpha $2 --costWeight $3 --chainNum $4
