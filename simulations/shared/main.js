9fcf7de25e957c82db2d230704371bcbccd30730
Robert Hawkins <hawkrobe@gmail.com>
2020-12-14 14:41:58 -0800

    add pure js implementation of L1 score

--- simulations/shared/main.js
+++ simulations/shared/main.js
@@ -13,8 +13,12 @@ var normalize = function(truth, sum) {
 var getLexiconElement = function(utt, target, params) {
   return _.min(_.map(utt.split('_'), function(word) {
     var uttLex = params.lexicon[word];
-    return _.includes(target.split('_'), uttLex) ? 1 : 0.00001;
+    return _.includes(target.split('_'), uttLex) ? 1 : 0;
   }));
+};
+
+var getUttCost = function(utt) {
+  return utt.split('_').length;
 };
 
 // We directly implement RSA without webppl to avoid overhead
@@ -22,8 +26,15 @@ var getLexiconElement = function(utt, target, params) {
 // => log(p) = log [ L(obj, utt) / \sum_{i} L(obj_i, utt) ]
 // if given object isn't in extension, some small probability of guessing it anyway
 var getL0Score = function(target, utt, params) {
-  var targetMeaning = getLexiconElement(utt, target, params);
   var noiseProb = 1 / params.context.length;
+  var targetMeaning = getLexiconElement(utt, target, params);
+
+  // Assume null object in context, so only possibility of picking object for which utterance
+  // is false comes from guessing
+  if(targetMeaning == 0) {
+    return Math.log(noiseProb * params.guessingEpsilon);
+  } 
+  
   var otherMeanings = 0;
   for(var i=0; i<params.context.length; i++){
     otherMeanings += getLexiconElement(utt, params.context[i], params);
@@ -37,11 +48,43 @@ var getL0Score = function(target, utt, params) {
 // => log P(utt | obj) = alpha * log P(obj | utt) - log(sum_i e^{alpha * log P(obj | utt)})
 var getS1Score = function(utt, targetObj, params) {
   // if given utterance is bad, some small probability of saying it anyway
+  var utility = function(utt) {
+    var inf = params.speakerAlpha * getL0Score(targetObj, utt, params);
+    return (1 - params.costWeight) * inf - params.costWeight * getUttCost(utt);
+  };
+
   var noiseProb = 1 / params.utterances.length;
-  var truth = params.speakerAlpha * getL0Score(targetObj, utt, params);
-  var sum = params.speakerAlpha * getL0Score(targetObj, params.utterances[0], params);
+  var truth = utility(utt);
+  var sum = utility(params.utterances[0]);
   for(var i=1; i< params.utterances.length; i++){
-    sum = numeric.logaddexp(sum, params.speakerAlpha * getL0Score(targetObj, params.utterances[i], params));
+    sum = numeric.logaddexp(sum, utility(params.utterances[i]));
+  }
+  var choiceProb = Math.exp(normalize(truth, sum));
+  return Math.log(noiseProb * params.guessingEpsilon +
+                  choiceProb * (1-params.guessingEpsilon));
+}
+
+// P(utt | obj) \propto e^{alpha * log P(obj | utt)}
+// => log P(utt | obj) = alpha * log P(obj | utt) - log(sum_i e^{alpha * log P(obj | utt)})
+var getL1Score = function(obj, utt, params) {
+  // if given utterance is bad, some small probability of saying it anyway
+  var utility = function(obj) {
+    return params.listenerAlpha * getS1Score(utt, obj, params);
+  };
+
+  // Assume null object in context, so only possibility of picking object for which utterance
+  // is false comes from guessing
+  var targetMeaning = getLexiconElement(utt, obj, params);
+  var noiseProb = 1 / params.context.length;
+  
+  if(targetMeaning == 0) {
+    return Math.log(noiseProb * params.guessingEpsilon);
+  } 
+
+  var truth = utility(obj);
+  var sum = utility(params.context[0]);
+  for(var i=1; i< params.context.length; i++){
+    sum = numeric.logaddexp(sum, utility(params.context[i]));
   }
   var choiceProb = Math.exp(normalize(truth, sum));
   return Math.log(noiseProb * params.guessingEpsilon +
@@ -51,5 +94,8 @@ var getS1Score = function(utt, targetObj, params) {
 module.exports = {
   readCSV: readCSV,
   getL0Score,
-  getS1Score
+  getL1Score,
+  getS1Score,
+  getUttCost,
+  getLexiconElement
 };
