b215e93f0a8d3ccdf5e4f4164976730ae741e651
Robert Hawkins <rxdh@stanford.edu>
2021-02-09 01:05:49 -0800

    add usePragmatics option

--- simulations/cogsci2018/test_model.sh
+++ simulations/cogsci2018/test_model.sh
@@ -1,4 +1,4 @@
 #!/bin/bash#
 rm $1.json
-/mnt/bucket/people/robertdh/.webppl/webppl run_simulation.wppl --require ./refModule/ --param-store --param-store file --param-id $1 --require webppl-csv -- --gameNum $1 --discountFactor $2 --coord $3
+webppl run_simulation.wppl --require ./refModule/ --param-store --param-store file --param-id $1 --require webppl-csv -- --gameNum $1 --usingPragmatics true --discountFactor $2 --coord $3
 
