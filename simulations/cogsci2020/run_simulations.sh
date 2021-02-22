3bc9663bf823efbf3b77e316e1df8b41f4ef5c03
Robert Hawkins <hawkrobe@gmail.com>
2021-02-22 01:36:14 -0800

    convert group simulations to use MCMC

--- simulations/cogsci2020/run_simulations.sh
+++ simulations/cogsci2020/run_simulations.sh
@@ -1,5 +1,3 @@
 #!/bin/bash#
 # parallel --bar --colsep ',' "sh ./run_simulations.sh {1} {2} {3} {4}" :::: input/reps.csv
-/mnt/bucket/people/robertdh/.webppl/webppl partnerspecificity_listener.wppl --require ./refModule/ --require webppl-csv -- --model $1 --alpha $2 --costWeight $3 --chainNum $4
-# /mnt/bucket/people/robertdh/.webppl/webppl partnerspecificity_speaker.wppl --require ./refModule/ --require webppl-csv -- --model $1 --alpha $2 --costWeight $3 --chainNum $4
-# /mnt/bucket/people/robertdh/.webppl/webppl network_simulation.wppl --require ./refModule/ --require webppl-csv -- --model $1 --alpha $2 --costWeight $3 --chainNum $4
+webppl network_simulation.wppl --require ../shared --require webppl-csv -- --model $1 --speakerAlpha $2 --listenerAlpha $3 --costWeight $4 --discountFactor $5 --guessingEpsilon $6 --chainNum $7
