c2c4f7b7d27f6b316adb6348273c8a168502c5b2
Robert Hawkins <hawkrobe@gmail.com>
2021-02-22 01:32:51 -0800

    add mixed condition back in

--- simulations/cogsci2018/input/gen_grid.py
+++ simulations/cogsci2018/input/gen_grid.py
@@ -6,22 +6,22 @@ folder = Path('.').rglob('game*.csv')
 files = [x.stem for x in folder]
 print(files)
 
-chainNum = 0
-with open('context_sensitivity_grid.csv', 'w') as csv_file :
-    writer = csv.writer(csv_file, delimiter=',')
-    for f in files :
-        for speakerAlpha in [1, 2, 4, 16, 64] :
-            for listenerAlpha in [1, 2, 4, 16, 64] : 
-                for discountFactor in [0.6, 0.8, 1] :
-                    for guessingEpsilon in [0.1, 0.01, 0.001, 0.00001] :
-                        writer.writerow([chainNum, f, speakerAlpha, listenerAlpha, discountFactor, guessingEpsilon])
-                        chainNum += 1
+# chainNum = 0
+# with open('context_sensitivity_grid.csv', 'w') as csv_file :
+#     writer = csv.writer(csv_file, delimiter=',')
+#     for f in files :
+#         for speakerAlpha in [1, 2, 4, 16, 64] :
+#             for listenerAlpha in [1, 2, 4, 16, 64] : 
+#                 for discountFactor in [0.6, 0.8, 1] :
+#                     for guessingEpsilon in [0.1, 0.01, 0.001, 0.00001] :
+#                         writer.writerow([chainNum, f, speakerAlpha, listenerAlpha, discountFactor, guessingEpsilon])
+#                         chainNum += 1
 
 chainNum = 200000
 with open('micro-grid.csv', 'w') as csv_file :
     writer = csv.writer(csv_file, delimiter=',')
     for f in files :
         for iterationNum in range(5) :
-            writer.writerow([chainNum, f, 10, 5, 0.8, 0.01])
+            writer.writerow([chainNum, f, 7, 7, 0.9, 0.01])
             chainNum += 1 
 
