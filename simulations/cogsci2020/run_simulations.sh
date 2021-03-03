9be03ac3dd33f8ca20f3f5820fd857de04fb05bb
Robert Hawkins <hawkrobe@gmail.com>
2021-03-02 18:34:47 -0800

    regenerate grid

--- simulations/cogsci2020/run_simulations.sh
+++ simulations/cogsci2020/run_simulations.sh
@@ -1,3 +1,3 @@
 #!/bin/bash#
-# parallel --bar --colsep ',' "sh ./run_simulations.sh {1} {2} {3} {4}" :::: input/reps.csv
+# parallel --bar --colsep ',' "sh ./run_simulations.sh {1} {2} {3} {4} {5} {6} {7}" :::: input/grid.csv
 webppl network_simulation.wppl --require ../shared --require webppl-csv -- --model $1 --speakerAlpha $2 --listenerAlpha $3 --costWeight $4 --discountFactor $5 --guessingEpsilon $6 --chainNum $7
